fc396d212766eb62ddebbde1d4f3778fb49a0102
John Ajiboye <ajiboye.o.john@gmail.com>
2018-06-28 09:39:38 +0300

    started with router implementation

--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ProductList from '@/components/shop/products/ProductList'
+import ShoppingCart from '@/components/shop/shopping-cart/ShoppingCart'
+import ProductDetails from '@/components/shop/products/ProductDetails'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'ProductList',
-      component: ProductList
-    }
+  routes: [{
+    path: '/',
+    name: 'ProductList',
+    component: ProductList
+  },
+  {
+    path: '/product/:id',
+    component: ProductDetails,
+    name: 'product'
+  },
+  {
+    path: '/cart',
+    component: ShoppingCart,
+    name: 'shoppingcart'
+  }
   ]
 })
