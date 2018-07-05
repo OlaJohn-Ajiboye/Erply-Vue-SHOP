991d2554f0ca660ac7f3281efa9f934cf890d5de
John Ajiboye <ajiboye.o.john@gmail.com>
2018-07-05 10:25:11 +0300

    Implemented Filter (not working perfectly yet)

--- vue-shop/src/components/shop/products/ProductList.vue
+++ vue-shop/src/components/shop/products/ProductList.vue
@@ -1,21 +1,41 @@
 <template>
   <div>
-    <div  id="hideMe" class="loader pad">..Loading Shop</div>
-      <div>
+   <div  id="hideMe" class="loader pad">..Loading Shop</div>
+   <div>
       <h3 class="font-weight-bold text-center pad">Products</h3>
-        <div class="col-sm-3" v-for="product in dataModel">
-        <Product :product="product" :key="product.id" track-by="id" />
+      <div class="row">
+         <div class="col-md-12">
+            <div class="col-md-6">
+               <div class="col-md-3">
+                  <div class="form-group">
+                     <label for="">Availability</label>
+                     <select class="form-control" @input="filterStock" v-model="instock">
+                        <option value="false">Out of Stock</option>
+                        <option value="true">In Stock</option>
+                     </select>
+                  </div>
+               </div>
+               <div class="col-md-3">
+                  <div class="form-group">
+                     <label for="">Filter by Store</label>
+                     <select class="form-control" @input="filterStore" v-model="store">
+                        <option value="Finland">Finland</option>
+                        <option value="Estonia">Estonia</option>
+                     </select>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-sm-3" v-for="product in dataModel">
+            <Product :product="product" :key="product.id" track-by="id" />
+          </div>
+               <div class="pagination-item ">
+                  <button class="btn btn-success btn-lg" :disabled="! showPrev || ! pagination" @click="renderList(params.current_page-1)">Prev</button>
+                  <button class="btn btn-success btn-lg" :disabled="! showNext || ! pagination" @click="renderList(params.current_page+1)">Next</button>
+               </div>
+            </div>
+         </div>
       </div>
-      <div class="container row">
-      <div class="col-md-6 col-md-offset-4">
-        <div class="pagination-item">
-						<button class="btn btn-success btn-sm" :disabled="! showPrev || ! pagination" @click="renderList(params.current_page-1)">Prev</button>
-						<button class="btn btn-success btn-sm" :disabled="! showNext || ! pagination" @click="renderList(params.current_page+1)">Next</button>
-					</div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -49,23 +69,23 @@ export default {
   methods: {
     renderList (pageNumber = 1) {
       this.dataModel = []
-			    if (this.model.length) {
-			    	this.params.from = pageNumber
+      if (this.model.length) {
+        this.params.from = pageNumber
         for (let i = this.start; i <= this.stop; i++) {
           this.dataModel.push(this.model[i])
         }
         this.params.current_page = this.params.from
         this.storeProducts(this.dataModel)// call storeProducts function and send dataModel to vuex
-			    }
+      }
     },
     ...mapActions(['storeProducts']),
     filterStore () {
       this.pagination = false
       var self = this
-		    const userFilter = this.model.filter(function (mode) {
-		       		return mode.store.toLowerCase().indexOf(self.store.toLowerCase()) >= 0
-		       	})
-		       	this.dataModel = userFilter
+	   const userFilter = this.model.filter(function (mode) {
+      		return mode.store.toLowerCase().indexOf(self.store.toLowerCase()) >= 0
+    	})
+      	this.dataModel = userFilter
     },
     filterStock () {
       this.pagination = false
@@ -73,7 +93,7 @@ export default {
 		    const userFilter = this.model.filter(function (mode) {
 		       		return mode.instock.toLowerCase().indexOf(self.instock.toLowerCase()) >= 0
 		       	})
-		       	this.dataModel = userFilter
+   	this.dataModel = userFilter
     }
   },
   computed: {
@@ -87,21 +107,66 @@ export default {
       if ((this.model.length - this.start) >= this.params.per_page) {
         return (this.params.from * this.params.per_page) - 1
       } else {
-	            return this.model.length - 1
-	       }
+        return this.model.length - 1
+      }
     },
     showNext () {
-		    return this.params.current_page < this.total
-		 },
+      return this.params.current_page < this.total
+    },
     showPrev () {
-		     return this.params.current_page > 1
+      return this.params.current_page > 1
     }
   }
 }
 </script>
 
 <style scoped>
+#hideMe {
+    -moz-animation: cssAnimation 0s ease-in 3s forwards;
+    /* Firefox */
+    -webkit-animation: cssAnimation 0s ease-in 3s forwards;
+    /* Safari and Chrome */
+    -o-animation: cssAnimation 0s ease-in 3s forwards;
+    /* Opera */
+    animation: cssAnimation 0s ease-in 3s forwards;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+}
+@keyframes cssAnimation {
+    to {
+        width:0;
+        height:0;
+        overflow:hidden;
+    }
+}
+@-webkit-keyframes cssAnimation {
+    to {
+        width:0;
+        height:0;
+        visibility:hidden;
+    }
+}
+
+.loader {
+    position: fixed;
+    font-size:25px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('https://raw.githubusercontent.com/OlaJohn-Ajiboye/Erply-Vue-SHOP/master/vue-shop/src/assets/loader.gif') 50% 50% no-repeat #e0e0e0;
+}
 .pad{
   padding: 0.5em
+}
+.pagination-item{
+  padding-bottom: 2em;
+  margin: 0 auto;
+}
+.btn{
+  padding-right: 2em;
+  padding-left: 2em;
+  text-align: center;
 }
 </style>
